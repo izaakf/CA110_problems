6d46188c528a5f237a1b2fc8b679ae9888e294de
izaakf <zakfreeman66@gmail.com>
2017-05-27 22:30:06 +0100

    complete

--- Toggleboard/toggleboard.rb
+++ Toggleboard/toggleboard.rb
@@ -1,78 +1,79 @@
+# Made by Zak Freeman
+
+print "input your board height: "
+board_height = gets.chomp.to_i
+print "input your board width: "
+board_width = gets.chomp.to_i
+
 board = []
-for board_y in 0..4
+
+for y in 0..board_height-1
   board.push([])
-  for board_x in 0..4
-    board[board_y].push(0)
+  board_width.times{board[y].push(0)}
+end
+
+def print_x(board, score)
+  print score * 2
+  board[0].each_index{|i| print i, score}
+  print "#{score}\r\n"
+end
+
+def print_y(board)
+  board.each_with_index{|n, i| puts i.to_s + '|' + n.each {|p| p }.join(' ') + '|' + i.to_s}
+end
+
+def print_board(board)
+  print_x(board, '_')
+  print_y(board)
+  print_x(board, '‾')
+end
+
+def shoot(board)
+  print "\r\nColumn? "
+  tile_x = gets.chomp.to_i
+  print "Row? "
+  tile_y = gets.chomp.to_i
+  begin
+    toggle_tiles(tile_y, tile_x, board)
+  rescue
+    Gem.win_platform? ? (system "cls") : (system "clear")
+    print "(#{tile_x}, #{tile_y}) out of bounds"
+    sleep(2)
   end
 end
 
+def toggle_tiles(tile_y, tile_x, board)
+  board[tile_y][tile_x] = ((board[tile_y][tile_x] - 1)* -1)
 
-def redraw_board(toggle_board)
-  for y in 0..3
-    for x in 0..3
-      toggle_board[y][x].remove
-    end
+  if tile_y == 0
+    board[tile_y + 1][tile_x] = ((board[tile_y + 1][tile_x] - 1)* -1)
+  elsif tile_y == board.length - 1
+    board[tile_y - 1][tile_x] = ((board[tile_y - 1][tile_x] - 1)* -1)
+  else
+    board[tile_y + 1][tile_x] = ((board[tile_y + 1][tile_x] - 1)* -1)
+    board[tile_y - 1][tile_x] = ((board[tile_y - 1][tile_x] - 1)* -1)
+  end
+
+  if tile_x == 0
+    board[tile_y][tile_x + 1] = ((board[tile_y][tile_x + 1] - 1)* -1)
+  elsif tile_x == board[tile_y].length - 1
+    board[tile_y][tile_x - 1] = ((board[tile_y][tile_x - 1] - 1)* -1)
+  else
+    board[tile_y][tile_x - 1] = ((board[tile_y][tile_x - 1] - 1)* -1)
+    board[tile_y][tile_x + 1] = ((board[tile_y][tile_x + 1] - 1)* -1)
   end
 end
 
-def adjacent_tiles(board, y_pos, x_pos)
-    board[y_pos][x_pos] = ((board[y_pos][x_pos] - 1)* -1)
-    board[y_pos + 1][x_pos] = ((board[y_pos + 1][x_pos] - 1)* -1)
-    board[y_pos - 1][x_pos] = ((board[y_pos - 1][x_pos] - 1)* -1)
-    board[y_pos][x_pos + 1] = ((board[y_pos][x_pos + 1] - 1)* -1)
-    board[y_pos][x_pos - 1] = ((board[y_pos][x_pos - 1] - 1)* -1)
-end
+while true
+  Gem.win_platform? ? (system "cls") : (system "clear")
 
-def check_if_over(board)
-  if board[0..3][0..3].all? {|x| x == array[0]}
-    alert("winner")
-  end
-end
-
-def draw_board(rect_x,rect_y, board)
-  # to test board:
-  # toggleboard[3][1] = 1
-
-  toggle_board = []
-  for board_y in 0..3
-    toggle_board.push([])
-    for board_x in 0..3
-      toggle_board[board_y].push(0)
-    end
+  if board.flatten.all? {|x| x == 1}
+    puts "COMPLETE!"
+    print_board(board)
+    break
   end
 
-  for y in 0..3
-    for x in 0..3
-      toggle_board[y][x] = stack top: rect_y, left: rect_x, width: 20, height: 20 do
-        background '00CED1'; unless board[y][x] == 1; background '26466D'; end
-        click do
-          # gets mouse x/y co-ordinates when a tile is clicked on the opponents board
-        |button, mouse_x, mouse_y|
-          # converts mouse_x and mouse_y to co-ordinates relevant to the board
-          y_pos = (mouse_y / 25) - 1
-          x_pos = (mouse_x / 25) - 1
-
-          if y_pos.between?(0, 3) && x_pos.between?(0, 3)
-            adjacent_tiles(board, y_pos, x_pos)
-            redraw_board(toggle_board)
-            draw_board(25, 25, board)
-            debug(board)
-            #check_if_over(board)
-            debug("x: #{x_pos}, y: #{y_pos}")
-          end
-        end
-      end
-
-    # increase x value to draw a column of squares
-    rect_x += 25
-    end
-    # resets x value and creates a new row to draw a column of squares
-    rect_x = 25
-    rect_y += 25
-  end
-end
-
-
-Shoes.app title:"toggleboard", width: 325, height: 300 do
-  draw_board(25, 25, board)
+  puts "current board: "
+  print_board(board)
+  shoot(board)
 end
